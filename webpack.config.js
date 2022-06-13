72d5367f6f285dbc61edec9a65c2cb7d8ec61021
Anna <anna-mitrofanova2008@yandex.ru>
2022-06-13 19:58:09 +0300

    fix: change index.html place

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const plugins = [
-  new HtmlWebpackPlugin({
-    template: './src/index.html'
-  })
+	new HtmlWebpackPlugin({
+		template: './index.html'
+	})
 ];
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  plugins,
+	entry: './index.js',
+	mode: 'development',
+	plugins,
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'assets/[hash][ext][query]',
-    clean: true,
-  },
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'assets/[hash][ext][query]',
+		clean: true
+	},
 
-  devtool: 'source-map',
+	devtool: 'source-map',
 
-  devServer: {
-    hot: true,
-  },
+	devServer: {
+		hot: true
+	},
 
-  module: {
-    rules: [
-      { test: /\.(html)$/, use: ['html-loader'] },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-    ]
-  }
-}
+	module: {
+		rules: [
+			{ test: /\.(html)$/, use: [ 'html-loader' ] },
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+				type: 'asset/resource'
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)$/i,
+				type: 'asset/resource'
+			}
+		]
+	}
+};
